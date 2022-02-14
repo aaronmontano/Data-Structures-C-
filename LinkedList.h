885825faab56d124ecd7044d0fb06db771265c33
Aaron Montano <aarchmon@Aarons-MacBook-Pro.local>
2022-02-13 22:04:56 -0800

    Updated Doxygen syntax. */

--- LinkedList.h
+++ LinkedList.h
@@ -6,13 +6,30 @@
 class LinkedList {
 
     private:
-        ListNode * head;
+        ListNode * head; /*!< Head of Linked List instance. */ 
     
     public:
 
-        LinkedList (void);
+        /**
+         * @brief Construct a new LinkedList object.
+         * @param None.
+         * @return LinkedList instance. 
+         */
+        LinkedList (void); 
 
-        void printList (void);
+        /**
+         * @brief Insert new Node data into Linked List. 
+         * @param newVal - Numerical value of new ListNode to be inserted. 
+         * @return None. 
+         */
+        void insertNode (int newVal); 
+
+        /**
+         * @brief Print out values of Linked List instance. 
+         * @param None.
+         * @return None. 
+         */
+        void printList (void); 
 
 };
 
--- ListNode.h
+++ ListNode.h
@@ -5,14 +5,24 @@
 
 class ListNode {
     public: 
-        int data;
-        ListNode * next; 
+        int data; /*!< Data value of Node (integer). */ 
+        ListNode * next; /*!< Pointer to next Node in Linked List. */
 
+        /**
+         * @brief Construct a new ListNode object (default constructor). 
+         * @param None. 
+         * @return ListNode instance (data set to 0). 
+         */
         ListNode () {
             this->data = 0;
             this->next = NULL;
         }
 
+        /**
+         * @brief Construct a new ListNode object (parameterized constructor). 
+         * @param data - Integer data to be held in Node. 
+         * @return None. 
+         */
         ListNode (int data) {
             this->data = data;
             this->next = NULL; 
